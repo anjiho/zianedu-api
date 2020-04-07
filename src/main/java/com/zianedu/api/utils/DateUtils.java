b3668370182f4d4dd2f032f43d0c5033a9a2c19f
안지호 <anjo0080@gmail.com>
2020-04-07 12:17:48 +0900

    ./

--- src/main/java/com/zianedu/api/utils/DateUtils.java
+++ src/main/java/com/zianedu/api/utils/DateUtils.java
@@ -151,30 +151,30 @@ public class DateUtils {
         System.out.println(newVodTitle2);
 
 //        String word = name;
-        System.out.println("utf-8(1) : " + new String(word.getBytes("utf-8"), "euc-kr"));
-        System.out.println("utf-8(2) : " + new String(word.getBytes("utf-8"), "ksc5601"));
-        System.out.println("utf-8(3) : " + new String(word.getBytes("utf-8"), "x-windows-949"));
-        System.out.println("utf-8(4) : " + new String(word.getBytes("utf-8"), "iso-8859-1"));
-
-        System.out.println("iso-8859-1(1) : " + new String(word.getBytes("iso-8859-1"), "euc-kr"));
-        System.out.println("iso-8859-1(2) : " + new String(word.getBytes("iso-8859-1"), "ksc5601"));
-        System.out.println("iso-8859-1(3) : " + new String(word.getBytes("iso-8859-1"), "x-windows-949"));
-        System.out.println("iso-8859-1(4) : " + new String(word.getBytes("iso-8859-1"), "utf-8"));
-
-        System.out.println("euc-kr(1) : " + new String(word.getBytes("euc-kr"), "ksc5601"));
-        System.out.println("euc-kr(2) : " + new String(word.getBytes("euc-kr"), "utf-8"));
-        System.out.println("euc-kr(3) : " + new String(word.getBytes("euc-kr"), "x-windows-949"));
-        System.out.println("euc-kr(4) : " + new String(word.getBytes("euc-kr"), "iso-8859-1"));
-
-        System.out.println("ksc5601(1) : " + new String(word.getBytes("ksc5601"), "euc-kr"));
-        System.out.println("ksc5601(2) : " + new String(word.getBytes("ksc5601"), "utf-8"));
-        System.out.println("ksc5601(3) : " + new String(word.getBytes("ksc5601"), "x-windows-949"));
-        System.out.println("ksc5601(4) : " + new String(word.getBytes("ksc5601"), "iso-8859-1"));
-
-        System.out.println("x-windows-949(1) : " + new String(word.getBytes("x-windows-949"), "euc-kr"));
-        System.out.println("x-windows-949(2) : " + new String(word.getBytes("x-windows-949"), "utf-8"));
-        System.out.println("x-windows-949(3) : " + new String(word.getBytes("x-windows-949"), "ksc5601"));
-        System.out.println("x-windows-949(4) : " + new String(word.getBytes("x-windows-949"), "iso-8859-1"));
+//        System.out.println("utf-8(1) : " + new String(word.getBytes("utf-8"), "euc-kr"));
+//        System.out.println("utf-8(2) : " + new String(word.getBytes("utf-8"), "ksc5601"));
+//        System.out.println("utf-8(3) : " + new String(word.getBytes("utf-8"), "x-windows-949"));
+//        System.out.println("utf-8(4) : " + new String(word.getBytes("utf-8"), "iso-8859-1"));
+//
+//        System.out.println("iso-8859-1(1) : " + new String(word.getBytes("iso-8859-1"), "euc-kr"));
+//        System.out.println("iso-8859-1(2) : " + new String(word.getBytes("iso-8859-1"), "ksc5601"));
+//        System.out.println("iso-8859-1(3) : " + new String(word.getBytes("iso-8859-1"), "x-windows-949"));
+//        System.out.println("iso-8859-1(4) : " + new String(word.getBytes("iso-8859-1"), "utf-8"));
+//
+//        System.out.println("euc-kr(1) : " + new String(word.getBytes("euc-kr"), "ksc5601"));
+//        System.out.println("euc-kr(2) : " + new String(word.getBytes("euc-kr"), "utf-8"));
+//        System.out.println("euc-kr(3) : " + new String(word.getBytes("euc-kr"), "x-windows-949"));
+//        System.out.println("euc-kr(4) : " + new String(word.getBytes("euc-kr"), "iso-8859-1"));
+//
+//        System.out.println("ksc5601(1) : " + new String(word.getBytes("ksc5601"), "euc-kr"));
+//        System.out.println("ksc5601(2) : " + new String(word.getBytes("ksc5601"), "utf-8"));
+//        System.out.println("ksc5601(3) : " + new String(word.getBytes("ksc5601"), "x-windows-949"));
+//        System.out.println("ksc5601(4) : " + new String(word.getBytes("ksc5601"), "iso-8859-1"));
+//
+//        System.out.println("x-windows-949(1) : " + new String(word.getBytes("x-windows-949"), "euc-kr"));
+//        System.out.println("x-windows-949(2) : " + new String(word.getBytes("x-windows-949"), "utf-8"));
+//        System.out.println("x-windows-949(3) : " + new String(word.getBytes("x-windows-949"), "ksc5601"));
+//        System.out.println("x-windows-949(4) : " + new String(word.getBytes("x-windows-949"), "iso-8859-1"));
 
 
         String paramStr = "USER_ID=test1234750&USER_KEY=12389&NAME="+newVodTitle+"&PWD=c7684301-+&GENDER=1&EMAIL=anjo0070@naver.com&TELEPHONE_MOBILE=010-6258-5228&RECV_SMS=1&RECV_EMAIL=1&AUTHORITY=10&ZIPCODE=12345&ADDRESS_ROAD=경기 성남시 수정구 탄리로 81&ADDRESS=302호";
